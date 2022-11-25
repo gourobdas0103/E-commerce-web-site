e37bf49ab551ba57166142f7af6deeafd7349b83
GOUROB DAS <90569569+gourabdas0987@users.noreply.github.com>
2022-11-26 01:08:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+ if(bar) {
+        bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if(close) {
+        close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+} 
+
+/*sproduct*/
+var mainImg = document.getElementById("mainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg [0].onclick = function(){
+    mainImg.src = smallimg[0].src;
+}
+smallimg [1].onclick = function(){
+    mainImg.src = smallimg[1].src;
+}
+smallimg [2].onclick = function(){
+    mainImg.src = smallimg[2].src;
+}
+smallimg [3].onclick = function(){
+    mainImg.src = smallimg[3].src;
+}
+/*scroll index file*/
+const bg = document.getElementById("hero")
+window.addEventListener('scroll', function(){
+    bg.style.backgroundSize = 100 - +window.pageYOffset/12+'vh';
+    bg.style.opacity = 5 - +window.pageYOffset/700+' ';
+})
